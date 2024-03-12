5616b25f3d57553dfeb39b85138c06fc291b8d30
Matheus <matheusfariasdasneves@gmail.com>
2024-03-12 17:37:39 -0300

    feat: add MenuPage

---
+++ src/pages/menu-page/index.jsx
@@ -0,0 +1,58 @@
+import { useContext, useEffect, useState } from "react";
+import { ScreenSizeContext } from "../../contexts/screen-size-context";
+
+import HeaderMenuMobile from "../../components/header-menu-mobile";
+import HeaderMenuDesktop from "../../components/header-menu-desktop";
+import Footer from "../../components/footer";
+import CardItem from "../../components/card-item";
+
+import { getItems } from "../../helpers/fakeApi";
+
+const MenuPage = () => {
+  const { desktop } = useContext(ScreenSizeContext);
+
+  const [hamburguerItems, setHamburguerItems] = useState([]);
+
+  useEffect(() => {
+    setHamburguerItems(getItems());
+  }, []);
+
+  return (
+    <div className="flex flex-col">
+      {desktop !== null ? (
+        desktop ? (
+          <HeaderMenuDesktop />
+        ) : (
+          <HeaderMenuMobile />
+        )
+      ) : null}
+
+      <main className="h-heightMinusHeader flex-1">
+        <div className="w-full h-full px-5 lg:px-[6.25em]">
+          <h1 className="mt-12 text-4xl font-black">
+            Nosso Cardápio<span className="text-primary">:</span>
+          </h1>
+
+          <div className="mt-16 grid grid-cols-1 xl:grid-cols-2">
+            {hamburguerItems.map((item) => {
+              return (
+                <CardItem
+                  key={item?.id}
+                  burguerName={item?.burguerName}
+                  price={item?.price}
+                  image={item?.image}
+                />
+              );
+            })}
+          </div>
+        </div>
+      </main>
+
+      <div className="mt-10">
+        <Footer />
+      </div>
+    </div>
+  );
+};
+
+export default MenuPage;
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 
 import HomePage from "../pages/home-page";
+import MenuPage from "../pages/menu-page";
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/menu" element={<MenuPage />} />
     </Routes>
   );
 };
