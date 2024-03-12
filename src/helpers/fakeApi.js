6d3646e8dacb993ef55652930025d7759fa836d3
Matheus <matheusfariasdasneves@gmail.com>
2024-03-12 17:34:54 -0300

    feat: add helper function getItems

---
+++ src/helpers/fakeApi.js
@@ -0,0 +1,28 @@
+export const getItems = () => {
+  return [
+    {
+      id: 1,
+      burguerName: "Burguer Picanha",
+      price: 70.0,
+      image: "./hamburguer-01.jpg",
+    },
+    {
+      id: 2,
+      burguerName: "Burguer Extra",
+      price: 40.0,
+      image: "./hamburguer-02.jpg",
+    },
+    {
+      id: 3,
+      burguerName: "Burguer X-Tudo",
+      price: 30.0,
+      image: "./hamburguer-03.jpg",
+    },
+    {
+      id: 4,
+      burguerName: "Burguer Recheado",
+      price: 40.0,
+      image: "./hamburguer-04.jpg",
+    },
+  ];
+};
