1d6c732ada090df0e07e1746da5b17f6d1455507
Matheus <matheusfariasdasneves@gmail.com>
2024-03-12 15:16:51 -0300

    refactor: refactor padding size

--- src/components/header-menu-desktop/index.jsx
+++ src/components/header-menu-desktop/index.jsx
@@ -2,7 +2,7 @@ import NavDesktop from "../nav-desktop";
 
 const HeaderMenuDesktop = () => {
   return (
-    <header className="w-full h-20 flex items-center justify-between bg-dark px-[6.25em]">
+    <header className="w-full h-20 flex items-center justify-between bg-dark px-5 lg:px-[6.25em]">
       <button>
         <img src="./logo.svg" alt="Logo Hamburguer House" />
       </button>
