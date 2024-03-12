1d2bd0d9c52279f708eae5bd067edf86860b4d57
Matheus <matheusfariasdasneves@gmail.com>
2024-03-12 16:26:39 -0300

    feat: add Footer

---
+++ src/components/footer/index.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="w-full h-40 bg-dark flex justify-center items-center">
+      <p className="text-light">BurguerHouse © 2024</p>
+    </footer>
+  );
+};
+
+export default Footer;
